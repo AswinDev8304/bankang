a08c1c230f8a9f0046b975f791dec0ab9c15f6c5
unknown <aswindeva2408304@gmail.com>
2023-05-30 12:06:18 +0530

    initial commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
  data="Happy banking with us !"
  pdata="Enter acc no"
+ servicedata:any
+ constructor(private rout:Router, private ds:DataService){
+
+ }
+ngOnInit():void{
+this.servicedata=this.ds.sdata
+console.log(this.servicedata);
+this.ds.smethod()
+
+}
  login(a:any){
   console.log(a.value);
   
   alert('login clicked')
+  this.rout.navigateByUrl('home')
  }
  accnoChange(event:any){
   console.log(event.target.value);
